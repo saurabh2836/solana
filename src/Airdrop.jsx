87ace6573fe4448175bbcef67a947713bce49144
saurabh kamble <saurabh2836@gmail.com>
2024-11-13 16:38:14 +0530

    adding new repo

---
+++ src/Airdrop.jsx
@@ -0,0 +1,6 @@
+
+export function Airdrop(){
+    return <div>
+        <h1>Airdrop</h1>
+    </div>
+}
---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+
+import '@solana/wallet-adapter-react-ui/styles.css';
+import { SendTokens } from './SendTokens';
+import { SignMessage } from './SignMessage';
+import {RequestAirdrop} from './RequestAirdrop';
+import { ShowSolBalance } from './ShowSolBalance';
+
+
+function App() {
+  return (
+      <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+          <WalletProvider wallets={[]} autoConnect>
+              <WalletModalProvider>
+                <div style={{ display: 'flex', justifyContent: "space-between" }}>
+                  <WalletMultiButton />
+                  <WalletDisconnectButton />
+                </div>
+                 <RequestAirdrop />
+                <ShowSolBalance /> 
+                <SignMessage />
+                <SendTokens />
+              </WalletModalProvider>
+          </WalletProvider>
+      </ConnectionProvider>
+  );
+}
+
+export default App
